d3a37041cd73ab7e5d45b4b1a05bc7964d73c98a
gvanhoy <basebzombie@gmail.com>
2017-09-19 18:49:18 -0700

    First block

---
+++ python/trained_model_classifier_vc.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# Copyright 2017 <+YOU OR YOUR COMPANY+>.
+# 
+# This is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+# 
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this software; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+# 
+
+import numpy
+from gnuradio import gr
+import pmt
+from sklearn.externals import joblib
+
+
+class trained_model_classifier_vc(gr.sync_block):
+    """
+    docstring for block trained_model_classifier_vc
+    """
+    def __init__(self, vlen, trained_model_filename):
+        gr.sync_block.__init__(
+            self,
+            name="trained_model_classifier_vc",
+            in_sig=[(numpy.complex64, vlen)],
+            out_sig=None)
+        self.message_port_register_out(pmt.intern('classification_info'))
+        self.classifier = joblib.load(trained_classier_file + '.pkl')
+
+    def work(self, input_items, output_items):
+        in0 = input_items[0]
+
+        for sample in in0:
+            self.classifier.predict(sample)
+            self.message_port_pub(
+                pmt.intern('classification_info'),
+                pmt.cons(pmt.intern('modulation'), pmt.to_pmt('BPSK')))
+
+        return len(input_items[0])
+
---
+++ swig/classify_swig.i
@@ -0,0 +1,13 @@
+/* -*- c++ -*- */
+
+#define CLASSIFY_API
+
+%include "gnuradio.i"			// the common stuff
+
+//load generated python docstrings
+%include "classify_swig_doc.i"
+
+%{
+%}
+
+
