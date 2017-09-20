f815520c2cd1202b6a138428041a61e3d30ec2cf
gvanhoy <basebzombie@gmail.com>
2017-09-20 00:47:08 -0700

    Debug

--- python/trained_model_classifier_vc.py
+++ python/trained_model_classifier_vc.py
@@ -48,11 +48,10 @@ class trained_model_classifier_vc(gr.sync_block):
     def work(self, input_items, output_items):
         in0 = input_items[0]
         # for x in range(0, len(input_items), self.vlen):
-        result = self.classifier.predict(in0[x:x + self.vlen])
+        result = self.classifier.predict(in0)
         for x in result:
             self.message_port_pub(
             pmt.intern('classification_info'),
             pmt.cons(pmt.intern('modulation'), pmt.to_pmt(self.result_map[x])))
-
         return len(input_items[0])
 
