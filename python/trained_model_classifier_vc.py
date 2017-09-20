09aa22b1feeff8da80eb55ce81f4f18668174513
gvanhoy <basebzombie@gmail.com>
2017-09-20 00:15:27 -0700

    Do not iterate

--- python/trained_model_classifier_vc.py
+++ python/trained_model_classifier_vc.py
@@ -46,7 +46,7 @@ class trained_model_classifier_vc(gr.sync_block):
 
     def work(self, input_items, output_items):
         in0 = input_items[0]
-        result = self.classifier.predict(sample)
+        result = self.classifier.predict(in0)
         self.message_port_pub(
             pmt.intern('classification_info'),
             pmt.cons(pmt.intern('modulation'), pmt.to_pmt(self.result_map[result])))
