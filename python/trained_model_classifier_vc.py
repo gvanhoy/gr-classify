17e5bc163066a86ec69a9244c70a75fb431124ab
gvanhoy <basebzombie@gmail.com>
2017-09-20 00:17:56 -0700

    Debug

--- python/trained_model_classifier_vc.py
+++ python/trained_model_classifier_vc.py
@@ -46,8 +46,8 @@ class trained_model_classifier_vc(gr.sync_block):
 
     def work(self, input_items, output_items):
         in0 = input_items[0]
+        print numpy.shape(in0)
         result = self.classifier.predict(in0)
-        print result
         # self.message_port_pub(
         #     pmt.intern('classification_info'),
         #     pmt.cons(pmt.intern('modulation'), pmt.to_pmt(self.result_map[result])))
